9072a91256d35fc8690665ba0255f09ed9ab11b0
wuyadong <wyd.npu@gmail.com>
2016-03-23 13:22:29 +0800

    update fabric file

--- fabfile.py
+++ fabfile.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 """
 fabric deploy file
-flower_ex 仅仅部署,并不安装,由各项目自己安装各种 venv
+fab build
+fab deploy -H 172.16.211.129 -u wangwa
 """
 
 from fabric.api import *
 
-env.user = 'wangwa'
-env.hosts = ['172.16.211.129']
+# env.user = 'wangwa'
+# env.hosts = ['172.16.211.129']
+env.use_ssh_config = True
 
 _NAME = 'flower_ex'
 _TAR_FILE = local('python setup.py --fullname', capture=True).strip() + '.tar.gz'
